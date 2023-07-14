6bc8f6f253d7f71947ff2a7f120db42ffbb7cb14
JennieJennae <chinonyejennie@gmail.com>
2023-07-14 05:36:02 -0700

    shell.h early beginnings

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef _SHELL_H_
+#define _SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
